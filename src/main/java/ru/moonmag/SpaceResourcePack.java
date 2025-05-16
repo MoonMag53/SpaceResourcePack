5dcfc1418ce7152e79b497e6edbd89575e08ca79
SpaceDev <mrdanyayt228@gmail.com>
2025-05-16 17:53:46 +0500

    Fix color on disable plugin

--- src/main/java/ru/moonmag/SpaceResourcePack.java
+++ src/main/java/ru/moonmag/SpaceResourcePack.java
@@ -25,13 +25,14 @@ public class SpaceResourcePack extends JavaPlugin implements Listener, TabExecut
         getCommand("ssrp").setTabCompleter(this);
         saveDefaultConfig();
         new ru.moonmag.Metrics(this, 25598);
-    }
+        new UpdateChecker(this).checkForUpdates();
+        }
 
     @Override
     public void onDisable() {
         getLogger().info("§x§f§f§7§c§0§0╔");
         getLogger().info("§x§f§f§7§c§0§0║ §fОтключение плагина...");
-        getLogger().info("§x§f§f§7§c§0§0║ §§x§f§f§0§0§0§0Плагин отключился! §fКодер: §x§f§f§6§e§0§0SpaceDev");
+        getLogger().info("§x§f§f§7§c§0§0║ §x§f§f§0§0§0§0Плагин отключился! §fКодер: §x§f§f§6§e§0§0SpaceDev");
         getLogger().info("§x§f§f§7§c§0§0║ §x§0§0§f§5§f§fh§x§0§0§f§4§f§ft§x§0§0§f§3§f§ft§x§0§0§f§2§f§fp§x§0§0§f§1§f§fs§x§0§0§e§f§f§f:§x§0§0§e§e§f§f/§x§0§0§e§d§f§f/§x§0§0§e§c§f§ft§x§0§0§e§b§f§f.§x§0§0§e§a§f§fm§x§0§0§e§9§f§fe§x§0§0§e§8§f§f/§x§0§0§e§7§f§fs§x§0§0§e§5§f§fp§x§0§0§e§4§f§fa§x§0§0§e§3§f§fc§x§0§0§e§2§f§fe§x§0§0§e§1§f§fs§x§0§0§e§0§f§ft§x§0§0§d§f§f§fu§x§0§0§d§e§f§fd§x§0§0§d§c§f§fi§x§0§0§d§b§f§fo§x§0§0§d§a§f§fm§x§0§0§d§9§f§fc");
         getLogger().info("§x§f§f§7§c§0§0╚");
     }
