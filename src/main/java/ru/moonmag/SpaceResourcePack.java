7410fc14ed959d7261cf8ce407fb941bfae285d8
SpaceDev <mrdanyayt228@gmail.com>
2026-03-10 13:48:01 +0500

    Фикс того, что можно было использовать только 2 строчки при кике

--- src/main/java/ru/moonmag/SpaceResourcePack.java
+++ src/main/java/ru/moonmag/SpaceResourcePack.java
@@ -10,6 +10,9 @@ import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class SpaceResourcePack extends JavaPlugin implements Listener, TabExecutor {
 
@@ -17,7 +20,7 @@ public class SpaceResourcePack extends JavaPlugin implements Listener, TabExecut
     public void onEnable() {
         getLogger().info("§x§f§f§7§c§0§0╔");
         getLogger().info("§x§f§f§7§c§0§0║ §fЗапуск плагина...");
-        getLogger().info("§x§f§f§7§c§0§0║ §x§0§0§f§f§1§7Плагин запустился! §fКодер: §x§f§f§6§e§0§0SpaceDev");
+        getLogger().info("§x§f§f§7§c§0§0║ §x§0§0§f§f§1§7Плагин запустился! §fКодер: §x§f§f§6§e§0§0m0onmag");
         getLogger().info("§x§f§f§7§c§0§0║ §x§0§0§f§5§f§fh§x§0§0§f§4§f§ft§x§0§0§f§3§f§ft§x§0§0§f§2§f§fp§x§0§0§f§1§f§fs§x§0§0§e§f§f§f:§x§0§0§e§e§f§f/§x§0§0§e§d§f§f/§x§0§0§e§c§f§ft§x§0§0§e§b§f§f.§x§0§0§e§a§f§fm§x§0§0§e§9§f§fe§x§0§0§e§8§f§f/§x§0§0§e§7§f§fs§x§0§0§e§5§f§fp§x§0§0§e§4§f§fa§x§0§0§e§3§f§fc§x§0§0§e§2§f§fe§x§0§0§e§1§f§fs§x§0§0§e§0§f§ft§x§0§0§d§f§f§fu§x§0§0§d§e§f§fd§x§0§0§d§c§f§fi§x§0§0§d§b§f§fo§x§0§0§d§a§f§fm§x§0§0§d§9§f§fc");
         getLogger().info("§x§f§f§7§c§0§0╚");
         getServer().getPluginManager().registerEvents(this, this);
@@ -26,13 +29,13 @@ public class SpaceResourcePack extends JavaPlugin implements Listener, TabExecut
         saveDefaultConfig();
         new ru.moonmag.Metrics(this, 25598);
         new UpdateChecker(this).checkForUpdates();
-        }
+    }
 
     @Override
     public void onDisable() {
         getLogger().info("§x§f§f§7§c§0§0╔");
         getLogger().info("§x§f§f§7§c§0§0║ §fОтключение плагина...");
-        getLogger().info("§x§f§f§7§c§0§0║ §x§f§f§0§0§0§0Плагин отключился! §fКодер: §x§f§f§6§e§0§0SpaceDev");
+        getLogger().info("§x§f§f§7§c§0§0║ §x§f§f§0§0§0§0Плагин отключился! §fКодер: §x§f§f§6§e§0§0m0onmag");
         getLogger().info("§x§f§f§7§c§0§0║ §x§0§0§f§5§f§fh§x§0§0§f§4§f§ft§x§0§0§f§3§f§ft§x§0§0§f§2§f§fp§x§0§0§f§1§f§fs§x§0§0§e§f§f§f:§x§0§0§e§e§f§f/§x§0§0§e§d§f§f/§x§0§0§e§c§f§ft§x§0§0§e§b§f§f.§x§0§0§e§a§f§fm§x§0§0§e§9§f§fe§x§0§0§e§8§f§f/§x§0§0§e§7§f§fs§x§0§0§e§5§f§fp§x§0§0§e§4§f§fa§x§0§0§e§3§f§fc§x§0§0§e§2§f§fe§x§0§0§e§1§f§fs§x§0§0§e§0§f§ft§x§0§0§d§f§f§fu§x§0§0§d§e§f§fd§x§0§0§d§c§f§fi§x§0§0§d§b§f§fo§x§0§0§d§a§f§fm§x§0§0§d§9§f§fc");
         getLogger().info("§x§f§f§7§c§0§0╚");
     }
@@ -70,16 +73,22 @@ public class SpaceResourcePack extends JavaPlugin implements Listener, TabExecut
         Player player = event.getPlayer();
 
         if (event.getStatus() == PlayerResourcePackStatusEvent.Status.DECLINED) {
-            String kickPrefix = Colorizer.colorize(getConfig().getString("KickPrefix"));
-            String line1 = Colorizer.colorize(getConfig().getString("Line-1"));
-            String line2 = Colorizer.colorize(getConfig().getString("Line-2"));
-            String kickMessage = Colorizer.colorize(getConfig().getString("KickMessage")).replace("%player%", player.getName());
 
-            player.kickPlayer(kickPrefix + " " + line1 + "\n" + line2);
+            List<String> lines = getConfig().getStringList("KickMessageLines");
 
-            for (Player onlinePlayer : getServer().getOnlinePlayers()) {
-                if (onlinePlayer.hasPermission("ssrp.admin")) {
-                    onlinePlayer.sendMessage(kickMessage);
+            String fullMessage = lines.stream()
+                    .map(line -> Colorizer.colorize(line.replace("%player%", player.getName())))
+                    .collect(Collectors.joining("\n"));
+
+            player.kickPlayer(fullMessage);
+
+            String notifyMessage = getConfig().getString("KickNotify")
+                    .replace("%player%", player.getName());
+            notifyMessage = Colorizer.colorize(notifyMessage);
+
+            for (Player p : getServer().getOnlinePlayers()) {
+                if (p.hasPermission("ssrp.admin")) {
+                    p.sendMessage(notifyMessage);
                 }
             }
         }
